9f78429e47b27b42772a4cdde88296c300283e52
sidharth1213 <siddharth.soni@cobold.in>
2023-09-12 10:55:16 +0530

    SS: Fixed layout and pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       </head>
       <body className={inter.className}>
-        <main className="container mx-auto">
+        <main className="xl:container mx-auto">
           <Navbar />
           {children}
           <Footer />
