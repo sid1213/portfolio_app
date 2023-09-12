e0a536885e6417f04611f98e1125c61c65e42170
sidharth1213 <siddharth.soni@cobold.in>
2023-09-12 15:22:45 +0530

    SS: Added new section for portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import About from "@/components/About";
 import HeroBanner from "@/components/HeroBanner";
+import Portfolio from "@/components/Portfolio";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between py-10 px-5">
       <HeroBanner />
       <About />
+      <Portfolio />
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
         blanditiis distinctio dolorem odit doloremque voluptas praesentium iusto
---
+++ components/Portfolio/index.tsx
@@ -0,0 +1,136 @@
+import React from "react";
+import { Tabs } from "antd";
+import type { TabsProps } from "antd";
+import "./index.scss";
+import Image from "next/image";
+
+const imagesData = [
+  {
+    imgCategory: "Art",
+    imgUrl: "rishab_about.png",
+  },
+  {
+    imgCategory: "laat",
+    imgUrl: "rishab_about.png",
+  },
+  {
+    imgCategory: "heart",
+    imgUrl: "rishab_about.png",
+  },
+  {
+    imgCategory: "PORTFOLIO",
+    imgUrl: "rishab_about.png",
+  },
+  {
+    imgCategory: "ALL",
+    imgUrl: "abc.png",
+  },
+  {
+    imgCategory: "ALL",
+    imgUrl: "bcd.png",
+  },
+  {
+    imgCategory: "PORTFOLIO",
+    imgUrl: "rishab_about.png",
+  },
+  {
+    imgCategory: "UI/UX",
+    imgUrl: "abc.png",
+  },
+  {
+    imgCategory: "UI/UX",
+    imgUrl: "abc.png",
+  },
+  {
+    imgCategory: "ALL",
+    imgUrl: "abc.png",
+  },
+  {
+    imgCategory: "ALL",
+    imgUrl: "abc.png",
+  },
+  {
+    imgCategory: "ALL",
+    imgUrl: "abc.png",
+  },
+  {
+    imgCategory: "ALL",
+    imgUrl: "abc.png",
+  },
+  {
+    imgCategory: "ALL",
+    imgUrl: "abc.png",
+  },
+  {
+    imgCategory: "ALL",
+    imgUrl: "abc.png",
+  },
+  {
+    imgCategory: "ALL",
+    imgUrl: "abc.png",
+  },
+  {
+    imgCategory: "ALL",
+    imgUrl: "abc.png",
+  },
+  {
+    imgCategory: "ALL",
+    imgUrl: "abc.png",
+  },
+];
+
+const tabs = [...new Set(imagesData.map((item) => item.imgCategory))];
+
+const items: TabsProps["items"] = tabs.map((category) => {
+  const filter = imagesData.filter((item) => item.imgCategory === category);
+  const tabData: React.JSX.Element[] = [];
+  filter.some((url, index) => {
+    if (index === 13) {
+      return true;
+    }
+    tabData.push(
+      <div key={url.imgCategory + index}>
+        <Image
+          src={"/" + url.imgUrl}
+          width={400}
+          height={400}
+          alt={url.imgUrl.split(".")[1]}
+          objectFit="contain"
+        />
+      </div>
+    );
+  });
+
+  return {
+    key: category,
+    label: category,
+    children: (
+      <div className="grid grid-cols-3 sm:grid-cols-4 gap-8">{tabData}</div>
+    ),
+  };
+});
+
+const Portfolio = () => {
+  return (
+    <section>
+      <div className=" p-5 w-full text-center">
+        <h1>Portfolio</h1>
+        <div className="mt-10">
+          <Tabs
+            defaultActiveKey={items[0].key}
+            items={items}
+            tabPosition="top"
+            className="tabs"
+          />
+        </div>
+        <div className="mt-5">
+          <button className="bg-[#FC5454] px-5 py-3 text-white font-semibold rounded-xl">
+            View more
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
